80fc74e8ee4835c57729bb2428033e19e8da7f20
ankushgarg1998 <1998anky@gmail.com>
2018-06-29 18:18:25 +0530

    Routes and Tests for DELETE /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -49,8 +51,28 @@ app.get('/todos/:id', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
 });
 
 module.exports = {app};
